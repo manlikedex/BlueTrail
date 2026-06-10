47358e751171870ef4bf58c8199d2ceca582ae16
Dex <19937974+manlikedex@users.noreply.github.com>
2026-06-10 12:32:28 +0100

    Add social feed, stories editor, comments, likes, update popup and auth improvements

--- components/AppScreen.tsx
+++ components/AppScreen.tsx
@@ -2,11 +2,13 @@
 
 import SideMenu from "./SideMenu";
 import BackButton from "./BackButton";
+import AppUpdatePopup from "./AppUpdatePopup";
 
 export default function AppScreen({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen bg-[#05070A] text-white">
       <SideMenu />
+      <AppUpdatePopup />
 
       <section className="mx-auto max-w-5xl px-4 pb-24 pt-6">
         <BackButton />
